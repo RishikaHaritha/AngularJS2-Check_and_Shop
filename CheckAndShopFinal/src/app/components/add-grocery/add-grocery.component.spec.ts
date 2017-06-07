24e893a94fb81200bb782b542381f4f2e4b94691
Krishna-Priya <lavanya.kondapally@gmail.com>
2017-06-07 20:37:47 +0530

    After adding cart and with modified UI (excepting homePage)

---
+++ CheckAndShopFinal/src/app/components/add-grocery/add-grocery.component.spec.ts
@@ -0,0 +1,17 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { AddGroceryComponent } from './add-grocery.component';
+
+describe('AddGroceryComponent', () => {
+  let component: AddGroceryComponent;
+  let fixture: ComponentFixture<AddGroceryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddGroceryComponent ]
+    })
+	
+	
